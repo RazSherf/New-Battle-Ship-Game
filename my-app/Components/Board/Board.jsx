f013d9e911f81027cf53759a355d96bf94aeb6fe
Raz Sherf <razsherf@gmail.com>
2022-12-14 10:31:36 +0200

    board added

--- my-app/Components/Board/Board.jsx
+++ my-app/Components/Board/Board.jsx
@@ -1,5 +1,37 @@
-import * as React from "react";
+import { useState } from "react";
+import style from "../Board/Board.module.css";
+import Square from "../Square/Square";
 
 export default function Board() {
-  return <div className={continer}></div>;
+  let column = [];
+  let rows = [];
+  let size = 11;
+
+  if (size == 10) {
+    column = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    rows = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+  }
+  if (size == 11) {
+    column = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+    rows = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"];
+  }
+  return (
+    <div className="battle-board-container">
+      <div className="columns-number-container">
+        {column.map((column) => (
+          <div className="columns-number" key={column}>
+            {column}
+          </div>
+        ))}
+      </div>
+      {rows.map((row, rowIndex) => (
+        <div className="row-letter-container" key={row}>
+          <span className="row-letter">{row}</span>
+          {column.map((column, index) => {
+            return <Square />;
+          })}
+        </div>
+      ))}
+    </div>
+  );
 }
